bffe3f3cf7ae8bff1442331290922e44b747c544
André Nogueira <andre.nogueira@epfl.ch>
2018-10-29 17:48:05 +0100

    fix: choose from graasp-users. and light-users. api subdomain
    close #28

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // todo: remove when adding more variables
 // eslint-disable-next-line import/prefer-default-export
 export const {
-  API_HOST = 'api.dev.graasp.eu',
+  GRAASP_HOST = 'local.graasp.eu:9123',
 } = process.env;
